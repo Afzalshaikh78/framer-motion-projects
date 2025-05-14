28e7310bf9fe1b50f1bd21eebf00e70b8a89f702
Afzalshaikh78 <afzalshaikh0771@gmail.com>
2025-05-14 19:56:45 +0530

    Update ScrollIndicator.tsx

--- src/Components/ScrollIndicator.tsx
+++ src/Components/ScrollIndicator.tsx
@@ -1,25 +1,33 @@
 import React from "react";
-import {motion, useTransform} from "framer-motion";
-import {useScroll} from "framer-motion";
+import { motion, useTransform } from "framer-motion";
+import { useScroll } from "framer-motion";
 import { line } from "motion/react-client";
 
 const ScrollIndicator = () => {
   const { scrollYProgress } = useScroll();
   const lineWidth = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
-  return <div className="h-[200vh] p-[20px]">
-    <motion.div className="fixed top-0 left-0 h-[5px] bg-red-500" style={{
-      width: lineWidth,
-     transition: 'width 0.0s ease',
-   }}/>
+  return (
+    <div className="h-[200vh] p-[20px]">
+      <motion.div
+        className="fixed top-0 left-0 h-[5px] bg-red-500"
+        style={{
+          width: lineWidth,
+          transition: "width 0.0s ease",
+        }}
+      />
 
-    <div className="mt-[50px]">
-      {[...Array(100)].map((_, index) => (
-        <p key={index} className="mt-[20px]">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus tenetur voluptatum cupiditate sit temporibus hic expedita minus sint similique perferendis obcaecati labore excepturi, accusamus autem pariatur explicabo. Tempore, sapiente facilis?
-        </p>
+      <div className="mt-[50px]">
+        {[...Array(50)].map((_, index) => (
+          <p key={index} className="mt-[20px]">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
+            tenetur voluptatum cupiditate sit temporibus hic expedita minus sint
+            similique perferendis obcaecati labore excepturi, accusamus autem
+            pariatur explicabo. Tempore, sapiente facilis?
+          </p>
         ))}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default ScrollIndicator;
